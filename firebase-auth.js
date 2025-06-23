e1b0ac52be060cd02c9c4c139c8423b11a26ae26
ShayanSheikh1936 <sheikhshayan082@gmail.com>
2025-06-24 02:02:00 +0500

    Shayan Sheikh id=294687

--- firebase-auth.js
+++ firebase-auth.js
@@ -1,15 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
-// import { firebaseConfig } from "./private";
+import { firebaseConfig } from "./private";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBY78RGi50TDkyYFRtrXMne74E9JBwFUr8",
-  authDomain: "restaurant-website-33112.firebaseapp.com",
-  projectId: "restaurant-website-33112",
-  storageBucket: "restaurant-website-33112.appspot.com",
-  messagingSenderId: "640390068562",
-  appId: "1:640390068562:web:678a387c7ebf9d11c2228b"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);

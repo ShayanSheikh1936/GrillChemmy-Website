631a5e5e1610f50555d7db2bf118774d2249b649
ShayanSheikh1936 <sheikhshayan082@gmail.com>
2025-06-23 23:12:00 +0500

    Shayan Sheikh id=294687

--- firebase-auth.js
+++ firebase-auth.js
@@ -1,24 +1,23 @@
- // Import the Firebase modules
  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
- import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
- import { firebaseConfig } from './private.js';
- 
- // Initialize Firebase
- const app = initializeApp(firebaseConfig);
- const auth = getAuth(app);
- 
- // Login function
- document.getElementById("submit").addEventListener("click", (e) => {
-   e.preventDefault();
- 
-   const email = document.getElementById("username").value;
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { firebaseConfig } from './private.js';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Login function
+document.getElementById("submit").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const email = document.getElementById("username").value;
  const password = document.getElementById("password").value;
- 
-   signInWithEmailAndPassword(auth, email, password)
-     .then((userCredential) => {
-       window.location.href = "admin.html";
-     })
-     .catch((error) => {
-       alert("Login Failed: " + error.message);
-     });
- });
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      window.location.href = "admin.html";
+    })
+    .catch((error) => {
+      alert("Login Failed: " + error.message);
+    });
+});

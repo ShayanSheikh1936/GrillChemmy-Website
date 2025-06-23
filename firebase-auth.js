eaf04b5f0d226ac01d684628bd45196f58f9b0b5
ShayanSheikh1936 <sheikhshayan082@gmail.com>
2025-06-24 02:15:24 +0500

    Shayan Sheikh id=294687

--- firebase-auth.js
+++ firebase-auth.js
@@ -1,7 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
-import { firebaseConfig } from './firebase-private.js';
-
+import { firebaseConfig } from './firebase-private';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);

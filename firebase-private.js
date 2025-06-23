8b9d3f1a90ebc93013e114dae54b8e981735d3af
ShayanSheikh1936 <sheikhshayan082@gmail.com>
2025-06-24 02:21:23 +0500

    Shayan Sheikh id=294687

--- firebase-private.js
+++ firebase-private.js
@@ -1,9 +1,9 @@
 // Firebase Config
- export const firebaseConfig = {
-    apiKey: "AIzaSyBY78RGi50TDkyYFRtrXMne74E9JBwFUr8",
-    authDomain: "restaurant-website-33112.firebaseapp.com",
-    projectId: "restaurant-website-33112",
-    storageBucket: "restaurant-website-33112.appspot.com",
-    messagingSenderId: "640390068562",
-    appId: "1:640390068562:web:678a387c7ebf9d11c2228b"
-  };
+//  export const firebaseConfig = {
+//     apiKey: "AIzaSyBY78RGi50TDkyYFRtrXMne74E9JBwFUr8",
+//     authDomain: "restaurant-website-33112.firebaseapp.com",
+//     projectId: "restaurant-website-33112",
+//     storageBucket: "restaurant-website-33112.appspot.com",
+//     messagingSenderId: "640390068562",
+//     appId: "1:640390068562:web:678a387c7ebf9d11c2228b"
+//   };
